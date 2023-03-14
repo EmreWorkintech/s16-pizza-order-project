c6b11435655372f9875bc7cb6127854cc25042a9
EmreSahiner <emre@journeyapp.com>
2023-03-14 11:18:32 +0300

    veritabanı işleri tamamlandı -waits testing

---
+++ data/seeds/002-users.js
@@ -0,0 +1,12 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').truncate()
+  await knex('users').insert([
+    {name: "Emre Şahiner", email: 'emre@wit.com.tr', password:'1234', role_id:1},
+    {name: "Erdem Günay", email: 'erdem@wit.com.tr', password:'1234', role_id:2}
+  ]);
+};
