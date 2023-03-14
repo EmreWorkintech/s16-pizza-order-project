f7a78c04a33c7d8fdfbd60b2a5fb8197edf440f6
EmreSahiner <emre@journeyapp.com>
2023-03-14 10:02:57 +0300

    json token eklendi

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../config/index');
 
 router.get('/', (req,res,next)=> {
     res.status(200).json({message: 'auth version 1 working'})
@@ -9,8 +11,26 @@ router.post('/register', (req,res,next)=> {
 })
 
 router.post('/login', (req,res,next)=> {
-    res.status(200).json({message: 'login working'})
+
+    //get user from the db
+    const user = {user_id: 1, role: "user"}; //TO-DO DB'den al
+
+    //password kontrolü
+
+    //generate token
+    const token = generateToken(user);
+    //return token
+    res.status(200).json({message: 'login working', token})
 })
+
+function generateToken(user){
+
+    const payload = {id: user.user_id, role: user.role};
+    const options = {expiresIn: '1h'};
+    const token = jwt.sign(payload, JWT_SECRET, options);
+
+    return token;
+}
 
 
 
--- api/server.js
+++ api/server.js
@@ -5,14 +5,17 @@ const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const ordersRouter = require('./orders/orders-router');
 
+const md = require('./auth/auth-middleware');
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-server.use('/api/orders', ordersRouter);
+server.use('/api/users', md.restricted, usersRouter);
+server.use('/api/orders', md.restricted, ordersRouter);
 
 
 server.use('*', (req,res)=> {
     res.status(404).json({message:'not found'})
 })
+
+
 
 module.exports = server;
